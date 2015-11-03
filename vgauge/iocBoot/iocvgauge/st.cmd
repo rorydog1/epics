80e6bc5d93d28dc74ca4f606f9f12445da366c3e
rory <rory@localhost.localdomain>
2015-11-03 14:32:45 +0000

    EDM fonts working and IOC talking to vacuum 937A controller. EDM displaying asynRecord GUI for 937

--- vgauge/iocBoot/iocvgauge/st.cmd
+++ vgauge/iocBoot/iocvgauge/st.cmd
@@ -12,15 +12,17 @@ dbLoadDatabase "dbd/vgauge.dbd"
 vgauge_registerRecordDeviceDriver pdbbase
 
 ## Configure devices
-drvAsynSerialPortConfigure("L0","/dev/ttyS0",0,0,0)
+   drvAsynSerialPortConfigure("L0","/dev/ttyS0",0,0,0)
    asynSetOption("L0",-1,"baud","9600")
    asynSetOption("L0",-1,"bits","8")
    asynSetOption("L0",-1,"parity","even")
    asynSetOption("L0",-1,"stop","1")
+   asynOctetSetOutputEos("L0",0,"\r")
+   asynOctetSetInputEos("L0",0,"\r")
 
 ## Load record instances
 #dbLoadRecords("db/xxx.db","user=roryHost")
-dbLoadRecords("db/asynRecord.db","P=norum:,R=asyn,PORT=L0,ADDR=24,IMAX=100,OMAX=100")
+dbLoadRecords("db/asynRecord.db","P=norum:,R=asyn,PORT=L0,ADDR=0,IMAX=100,OMAX=100")
 
 cd "${TOP}/iocBoot/${IOC}"
 iocInit
