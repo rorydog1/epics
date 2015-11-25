9c67e232d5f8f6f532b33988738c2a7d8134e4e3
rory <rory@localhost.localdomain>
2015-11-25 10:48:46 +0000

    Working record and stream device for the MKS in vgauge

---
+++ gp08_ioc01/gp08_ioc01App/src/O.linux-x86_64/gp08_ioc01.dbd.d
@@ -0,0 +1,49 @@
+# DO NOT EDIT: This file created by mkmf.pl,v 1.5 2002/03/25 21:33:24 jba Exp $ 
+
+../O.Common/gp08_ioc01.dbd : /home/rory/Desktop/epics/dbd/base.dbd
+../O.Common/gp08_ioc01.dbd : /home/rory/Desktop/epics/dbd/menuGlobal.dbd
+../O.Common/gp08_ioc01.dbd : /home/rory/Desktop/epics/dbd/menuConvert.dbd
+../O.Common/gp08_ioc01.dbd : /home/rory/Desktop/epics/dbd/aaiRecord.dbd
+../O.Common/gp08_ioc01.dbd : /home/rory/Desktop/epics/dbd/aaoRecord.dbd
+../O.Common/gp08_ioc01.dbd : /home/rory/Desktop/epics/dbd/aiRecord.dbd
+../O.Common/gp08_ioc01.dbd : /home/rory/Desktop/epics/dbd/aoRecord.dbd
+../O.Common/gp08_ioc01.dbd : /home/rory/Desktop/epics/dbd/aSubRecord.dbd
+../O.Common/gp08_ioc01.dbd : /home/rory/Desktop/epics/dbd/biRecord.dbd
+../O.Common/gp08_ioc01.dbd : /home/rory/Desktop/epics/dbd/boRecord.dbd
+../O.Common/gp08_ioc01.dbd : /home/rory/Desktop/epics/dbd/calcRecord.dbd
+../O.Common/gp08_ioc01.dbd : /home/rory/Desktop/epics/dbd/calcoutRecord.dbd
+../O.Common/gp08_ioc01.dbd : /home/rory/Desktop/epics/dbd/compressRecord.dbd
+../O.Common/gp08_ioc01.dbd : /home/rory/Desktop/epics/dbd/dfanoutRecord.dbd
+../O.Common/gp08_ioc01.dbd : /home/rory/Desktop/epics/dbd/eventRecord.dbd
+../O.Common/gp08_ioc01.dbd : /home/rory/Desktop/epics/dbd/fanoutRecord.dbd
+../O.Common/gp08_ioc01.dbd : /home/rory/Desktop/epics/dbd/longinRecord.dbd
+../O.Common/gp08_ioc01.dbd : /home/rory/Desktop/epics/dbd/longoutRecord.dbd
+../O.Common/gp08_ioc01.dbd : /home/rory/Desktop/epics/dbd/mbbiRecord.dbd
+../O.Common/gp08_ioc01.dbd : /home/rory/Desktop/epics/dbd/mbbiDirectRecord.dbd
+../O.Common/gp08_ioc01.dbd : /home/rory/Desktop/epics/dbd/mbboRecord.dbd
+../O.Common/gp08_ioc01.dbd : /home/rory/Desktop/epics/dbd/mbboDirectRecord.dbd
+../O.Common/gp08_ioc01.dbd : /home/rory/Desktop/epics/dbd/permissiveRecord.dbd
+../O.Common/gp08_ioc01.dbd : /home/rory/Desktop/epics/dbd/selRecord.dbd
+../O.Common/gp08_ioc01.dbd : /home/rory/Desktop/epics/dbd/seqRecord.dbd
+../O.Common/gp08_ioc01.dbd : /home/rory/Desktop/epics/dbd/stateRecord.dbd
+../O.Common/gp08_ioc01.dbd : /home/rory/Desktop/epics/dbd/stringinRecord.dbd
+../O.Common/gp08_ioc01.dbd : /home/rory/Desktop/epics/dbd/stringoutRecord.dbd
+../O.Common/gp08_ioc01.dbd : /home/rory/Desktop/epics/dbd/subRecord.dbd
+../O.Common/gp08_ioc01.dbd : /home/rory/Desktop/epics/dbd/subArrayRecord.dbd
+../O.Common/gp08_ioc01.dbd : /home/rory/Desktop/epics/dbd/waveformRecord.dbd
+../O.Common/gp08_ioc01.dbd : /home/rory/Desktop/epics/dbd/devSoft.dbd
+../O.Common/gp08_ioc01.dbd : /home/rory/Desktop/epics/dbd/menuAlarmSevr.dbd
+../O.Common/gp08_ioc01.dbd : /home/rory/Desktop/epics/dbd/menuAlarmStat.dbd
+../O.Common/gp08_ioc01.dbd : /home/rory/Desktop/epics/dbd/menuCompress.dbd
+../O.Common/gp08_ioc01.dbd : /home/rory/Desktop/epics/dbd/menuFtype.dbd
+../O.Common/gp08_ioc01.dbd : /home/rory/Desktop/epics/dbd/menuIvoa.dbd
+../O.Common/gp08_ioc01.dbd : /home/rory/Desktop/epics/dbd/menuOmsl.dbd
+../O.Common/gp08_ioc01.dbd : /home/rory/Desktop/epics/dbd/menuPini.dbd
+../O.Common/gp08_ioc01.dbd : /home/rory/Desktop/epics/dbd/menuPriority.dbd
+../O.Common/gp08_ioc01.dbd : /home/rory/Desktop/epics/dbd/menuScan.dbd
+../O.Common/gp08_ioc01.dbd : /home/rory/Desktop/epics/dbd/menuSimm.dbd
+../O.Common/gp08_ioc01.dbd : /home/rory/Desktop/epics/dbd/menuYesNo.dbd
+../O.Common/gp08_ioc01.dbd : /home/rory/Desktop/epics/dbd/dbCommon.dbd
+
+
+../O.Common/gp08_ioc01Include.dbd : ../Makefile
